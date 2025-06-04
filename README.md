6ee471787426172d3fe0773bfa6b7aac76b971da
dongshengye <934316559@qq.com>
2025-06-04 16:33:54 +0800

    增加nginx配置

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 企业员工管理系统
 
-XJTU 敏捷web开发
+XJTU 敏捷web开发 vue 前端
--- vue.config.js
+++ vue.config.js
@@ -18,3 +18,17 @@ module.exports={
   },
   publicPath: './'
 }
+
+/**以下是nginx配置文件
+ * 
+ *     # 转发 API 请求到 localhost:8000
+    location /api/ {
+        proxy_pass http://localhost:8000;  # 将请求转发到本地的8000端口
+        proxy_set_header Host $host;       # 保持 Host 头
+        proxy_set_header X-Real-IP $remote_addr;  # 保持客户端真实IP
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;  # 保持原始客户端IP
+        proxy_set_header X-Forwarded-Proto $scheme;  # 保持请求协议
+        proxy_redirect off;  # 禁止重定向
+        rewrite ^/api(/.*)$ /api/v1$1 break;  # 将 /api 重写为 /api/v1
+    }
+ */
